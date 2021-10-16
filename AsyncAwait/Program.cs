6a78676072412b63af396eefd6039b80a4037eea
Cong MA <cong.ma@pointsbet.com>
2021-10-16 22:26:39 +1100

    Submit demo code for async await.

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CSharpConcept
+{
+    class Program
+    {
+        private static HttpClient _httpClient = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+            var repeats = 1000;
+
+            #region CPU intensive job
+            Console.WriteLine("Start CPU intensive job");
+            RepeatingJobRunner(repeats, Divide);
+            watch.Stop();
+            Console.WriteLine("Synchronous takes {0}s.", watch.Elapsed.TotalSeconds);
+
+            // Asynchronous with separate threads
+            watch.Restart();
+
+            await RepeatingJobRunnerAsync(repeats, Divide, true);
+            watch.Stop();
+            Console.WriteLine("Asynchronous with separate threads takes {0}s.", watch.Elapsed.TotalSeconds);
+
+            // Asynchronous without separate threads
+            watch.Restart();
+
+            await RepeatingJobRunnerAsync(repeats, Divide, false);
+            watch.Stop();
+            Console.WriteLine("Asynchronous without separate threads takes {0}s.", watch.Elapsed.TotalSeconds);
+            Console.WriteLine("End");
+            Console.WriteLine();
+            #endregion
+
+            #region Network IO intensive job
+
+            Console.WriteLine("Start Network IO intensive job");
+            repeats = 100;
+            watch.Restart();
+            RepeatingJobRunner(repeats, GetWebPage);
+            watch.Stop();
+            Console.WriteLine("Synchronous takes {0}s.", watch.Elapsed.TotalSeconds);
+
+            // Asynchronous without separate threads
+            watch.Restart();
+
+            await RepeatingJobRunnerAsync(repeats, GetWebPage, false);
+            watch.Stop();
+            Console.WriteLine("Asynchronous without separate threads takes {0}s.", watch.Elapsed.TotalSeconds);
+
+            // Asynchronous with separate threads
+            watch.Restart();
+
+            await RepeatingJobRunnerAsync(repeats, GetWebPage, true);
+            watch.Stop();
+
+            Console.WriteLine("Asynchronous with separate threads takes {0}s.", watch.Elapsed.TotalSeconds);
+            Console.WriteLine("End");
+            #endregion
+        }
+
+        private static void RepeatingJobRunner(int repeatTimes, Func<Task> func)
+        {
+            for (int i = 0; i < repeatTimes; i++)
+            {
+                func().Wait();
+            }
+        }
+
+        private static async Task RepeatingJobRunnerAsync(int repeatTimes, Func<Task> func, bool useSeparateThread)
+        {
+            var tasks = new List<Task>();
+            for (int i = 0; i < repeatTimes; i++)
+            {
+                if (useSeparateThread)
+                {
+                    tasks.Add(Task.Run(() => func()));
+                }
+                else
+                {
+                    tasks.Add(func());
+                }
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        private static Task Divide()
+        {
+            var a = 10;
+            var b = 3.0m;
+
+            for (var i = 0; i < 10000; i++)
+            {
+                decimal.Parse((a / b).ToString());
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static Task GetWebPage()
+        { 
+            return _httpClient.GetAsync("https://www.google.com");
+        }
+    }
+}
---
+++ AsyncAwait/readme.md
@@ -0,0 +1,12 @@
+﻿# Summary
+The intent of this project is to compare the performance of synchronous and asynchronous operations under different scenarios in .NET applications.
+
+# Scenarios
+## CPU intensive
+In this case, the sync/async operation is a CPU intensive task. I use the divide operation here.
+
+## Network intensive
+In this case, the cost is mostly on the network side. In this example, it simply loads the home page of `https://www.google.com` many times.
+
+## Disk IO intensive
+In this case, the operation is to write/read data from harddisk.
